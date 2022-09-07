e3a2bf5d5430acaf7fc39ba62b575149e7b270f9
eternalweary27 <99358134+eternalweary27@users.noreply.github.com>
2022-09-07 02:48:27 +0100

    Create urahara_quotes.py

---
+++ urahara_quotes.py
@@ -0,0 +1,40 @@
+Noooo, there’s no way I, a mere handsome and sexy shopkeeper, could possibly have bankai!
+There’s a lot of products that you can buy with money…but my love is the only limited product that only you can have.
+That wasn’t very nice. I do believe you’ve killed my hat.
+I’m sure it’s just some pretty girl talking about me somewhere.
+Aizen-sama will get maaaad at youuuu!
+Oh, it IS a chore. Risking your life and keeping busy are all the same!
+Don’t use others as a reason for walking into death’s arms
+You give me too much credit. These days, I’m just a plain old candy-store owner
+You’re my fuzzy little troublemaker, yes you are. Now who’s your daddy? Hmm?
+If you're thinking that this message is like some written in the victim's own blood cliché from some TV show...then you obviously have no sense of humor!
+Chad, have you ever heard of the worf-effect? It's when a character is really strong but always loses fights to show how strong a bad guy is.
+You were taking it so well, wasn't expecting an elbow to the face.
+In the time that I haven't seen you, you've taken up some interesting fashion.
+Now yell as loud as you can: amazing headband of justice in place, amazing armour of justice protect me now!
+Aghhh, you broke my nose!
+Only those swallowed up by ignorance and fear who take false steps, shall plunge us into the waters called “destiny”.
+I’ll drive you into a corner and make you remember that attack.
+Name is Urahara Kisuke, the humble owner of a small neighbourhood candy store.
+Scream Benihime!
+This episode was brought to by Portable Gigai, now available for the low price of only $19.95!
+A warrior who has lost his ability to fight is only going to be in the way.
+Lately, I've been looking at these two guys only. As expected, I see flowers when there are women in the workplace...the atmosphere.
+Don't give up on the whole barrel just cause of a few bad apples.
+What's necessary in a fight isn't fear. Nothing can be born of that.
+If we want to witness something nobody has ever seen before, we'll create it ourselves. That's how us scientists are.
+If I prepare a thousand options, all I need is for one of them to work. So I make a habit of preparing anything and everything that shows any kind of promise. That's just how I work.
+Captain Kurotsuchi is a genius ya know, but not as great as me.
+Oh so you do remember me! Always a pleasure to see you.
+Interesting how the fear of eminent death can change one's mind.
+Being forewarned is being forearmed.
+Heheheheh [very stoned]
+It may be hard to believe, but there are some things in this world that even I don't know.
+Looks like you got your ass stuffed full of a lethal dose from where I'm sitting.
+This movie needs more Yoruichi
+I have lots of those hats, maybe a hundred of them. By the way, I have a lot of identical robes and clogs and jinbei too.
+Hado number 31 Shock a Hoe!
+Changing what makes you mad and what doesn't is basically the same thing as 'changing your own feelings'. Right?
+How am I still alive? How indeed.
+If the linchpin is lost, everything will soon collapse.
+I'm a Reaper Girl In the Reaper World, Life is peachy, I'm Yoruichi. I got purple hair, black spandex everywhere, my reputation feline transformation
